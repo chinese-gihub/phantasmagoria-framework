95e62a4456ce8d647aeeb0e75d51cf63f0c9d4b8
William <mrpimpunicorn@users.noreply.github.com>
2018-02-28 16:39:02 -0500

    Added project files

---
+++ source/Phantasmagoria.Framework/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Runtime;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Phantasmagoria.Framework.Audio")]
+[assembly: InternalsVisibleTo("Phantasmagoria.Framework.Game")]
+[assembly: InternalsVisibleTo("Phantasmagoria.Framework.Graphics")]
