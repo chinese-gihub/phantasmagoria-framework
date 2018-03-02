eaf46a104f00e7425ff3c931870cc4de294d597f
William <mrpimpunicorn@users.noreply.github.com>
2018-03-02 14:11:05 -0500

    Added event handler extensions class

---
+++ source/Phantasmagoria.Framework/EventHandlerExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Phantasmagoria.Framework
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	internal static class EventHandlerExtensions
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="eventHandler"></param>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		public static void TryRaise(this EventHandler eventHandler, object sender, EventArgs e)
+		{
+			if ((sender == null) || (e == null))
+				throw new ArgumentNullException();
+
+			eventHandler?.Invoke(sender, e);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="eventHandler"></param>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		public static void TryRaise<T>(this EventHandler<T> eventHandler, object sender, T e)
+		{
+			if ((sender == null) || (e == null))
+				throw new ArgumentNullException();
+
+			eventHandler?.Invoke(sender, e);
+		}
+	}
+}
