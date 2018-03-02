289277ac80446da5042dc0871b0ae62d53fb2835
William <mrpimpunicorn@users.noreply.github.com>
2018-03-02 14:34:04 -0500

    Added game stopwatch event args class

---
+++ source/Phantasmagoria.Framework.Game/GameStopwatchEventArgs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Phantasmagoria.Framework
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	[Serializable]
+	internal class GameStopwatchEventArgs
+		: EventArgs
+	{
+		private readonly GameTime gameTime;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public GameTime GameTime
+		{
+			get { return this.gameTime; }
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="gameTime"></param>
+		public GameStopwatchEventArgs(GameTime gameTime)
+			: base()
+		{
+			this.gameTime = gameTime;
+		}
+	}
+}
