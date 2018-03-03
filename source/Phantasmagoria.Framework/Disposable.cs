5177a7ef045870f9cb0c10f8f4ab130ee635d22e
William <mrpimpunicorn@users.noreply.github.com>
2018-03-03 01:15:33 -0500

    Added disposable class

---
+++ source/Phantasmagoria.Framework/Disposable.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Phantasmagoria.Framework
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public abstract class Disposable
+		: IDisposable
+	{
+		private bool isDisposed;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public event EventHandler<DisposableEventArgs> Disposing;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public bool IsDisposed
+		{
+			get { return this.isDisposed; }
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		protected Disposable()
+		{
+			this.isDisposed = false;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public void Dispose()
+		{
+			TryDispose(true);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		protected void ThrowIfDisposed()
+		{
+			if (this.isDisposed)
+			{
+				// Get the name for the object.
+				var objectName = GetType().Name;
+
+				throw new ObjectDisposedException(objectName);
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="disposing"></param>
+		protected virtual void Dispose(bool disposing) { }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="disposing"></param>
+		private void TryDispose(bool disposing)
+		{
+			if (this.isDisposed)
+				return;
+
+			// Raise the disposing event.
+			var args = new DisposableEventArgs(disposing);
+			Disposing.TryRaise(this, args);
+			
+			Dispose(disposing);
+			GC.SuppressFinalize(this);
+
+			this.isDisposed = true;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		~Disposable()
+		{
+			TryDispose(false);
+		}
+	}
+}
