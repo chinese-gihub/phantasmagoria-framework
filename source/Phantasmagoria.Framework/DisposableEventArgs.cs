6fdcee3cd045ec9cf0852e28f0800d315fcc0a6d
William <mrpimpunicorn@users.noreply.github.com>
2018-03-03 01:14:48 -0500

    Added disposable event args class

---
+++ source/Phantasmagoria.Framework/DisposableEventArgs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Phantasmagoria.Framework
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	[Serializable]
+    public class DisposableEventArgs
+		: EventArgs
+    {
+		private readonly bool disposing;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public bool Disposing
+		{
+			get { return this.disposing; }
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="disposing"></param>
+		public DisposableEventArgs(bool disposing)
+			: base()
+		{
+			this.disposing = disposing;
+		}
+    }
+}
